1dda94e300c15059e524dc349e7efd3740d5d258
andrew-kwong <andrew.kwong@knights.ucf.edu>
2020-04-10 20:30:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,14 +2,7 @@ import * as firebase from "firebase";
 import '@firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdMwtjqAdF3IPP4qz3sw8wcospOR2Ebk4",
-  authDomain: "study-spot-finder-1aa13.firebaseapp.com",
-  databaseURL: "https://study-spot-finder-1aa13.firebaseio.com",
-  projectId: "study-spot-finder-1aa13",
-  storageBucket: "study-spot-finder-1aa13.appspot.com",
-  messagingSenderId: "1049767455344",
-  appId: "1:1049767455344:web:d608124847bf49c202e2b5",
-  measurementId: "G-J5RGCSDC14"
+  
 };
 
 firebase.initializeApp(firebaseConfig);
